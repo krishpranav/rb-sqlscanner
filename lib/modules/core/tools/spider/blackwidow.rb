6fdea6dd114993c95c67e667036c099d5d0f794e
krishpranav <krisna.pranav@gmail.com>
2021-05-13 10:57:56 +0530

    update black widow

---
+++ lib/modules/core/tools/spider/blackwidow.rb
@@ -0,0 +1,37 @@
+require_relative '../../../../../lib/modules/core/settings'
+
+include Settings
+
+#
+# Recursive spider search, GRABS EVERY URL AVAILABLE
+#
+module BlackWidow
+
+  class RecursiveSpider
+
+    #
+    # Extracts all the links from a url
+    #
+    def pull_links(url)
+      data = Settings::ProgramSettings.new.parse(url, 'html', 0)
+      return URI.extract(data, ['http', 'https'])
+    end
+
+    #
+    # Extracts the links from the links that were extracted making even more links, it's pretty sexy..
+    #
+    def follow_links(url_arr)
+      url_arr.each do |link|
+        search = Settings::ProgramSettings.new.parse(link, 'html', 0)
+        data = URI.extract(search, %w(http https))
+        data.each do |url|
+          FORMAT.info("Found redirect: #{url}")
+          File.open("#{PATH}/tmp/blackwidow_log.txt", "a+") { |to_search| to_search.write(url + "\n") }
+          sleep(0.2)
+        end
+      end
+    end
+
+  end
+
+end
